2b9587a81c6a1eef3bb9625b73c1d8caf7f96b9b
allenx <986458768@qq.com>
2020-01-08 09:12:45 +0800

    1. add func start, stop and reset 2. add close event and result check.

--- main.py
+++ main.py
@@ -2,26 +2,36 @@
 # -*- coding! utf-8 -*-
 
 """
-Main window to manage and run RPA projects
+Just for fun
 author: allenx
 """
 
-import os
 import sys
 import time
-from os.path import join, expanduser, basename, exists
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel, QPushButton, QCheckBox, \
-    QTableWidgetItem, QFileDialog, QAbstractItemView, QMessageBox, QListWidgetItem
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QTableWidgetItem, QListWidgetItem, QMessageBox
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QTimer
 from PyQt5.QtGui import QFont
 from window.window import Ui_Form
 
+NAME_LIST = ['sophia he', 'dora qiu', 'amber wei', 'lou lu', 'rachel zhuo', 'gavin chen',
+             'molly wang', 'helena xu', 'sadie wen', 'simona zhang', 'arthur shu', 'marian chen',
+             'alisa tang', 'allen xia', 'rebecca luo', 'cristiney zhang', 'travis zou', 'cecil xu',
+             'hermona fang', 'ellie ai', 'debbie ye', 'perla qiu', 'poppy wu', 'trista hu',
+             'chloe sha', 'scarlett yang', 'katty pu', 'olivia cui', 'gloria qiu', 'dylan dai',
+             'kate zhang', 'stephen pan', 'cathy yang', 'cloud lei'
+             ]
+
+GROUP_LIST = []
+THREAD_RUN = False
+RESULT = False
+SPEED = 0.05
+
 
 class MainWindow(QWidget, Ui_Form):
+    """Just for fun"""
 
-    """Main window class used to manage over RPA_projects."""
-
-    num = 0
+    row = 0
+    col = 0
 
     def __init__(self):
         super().__init__()
@@ -29,9 +39,12 @@ class MainWindow(QWidget, Ui_Form):
         self.center()
         self.init_item()
         self.thread = WheelThread()
+        self.timer = QTimer()
+
         self.StartButton.clicked.connect(self.wheel_run)
+        self.ResetButton.clicked.connect(self.reset)
         self.thread.sin_out.connect(self.list_add)
-        self.ResetButton.clicked.connect(self.print_selected)
+        self.timer.timeout.connect(self.group_add)
 
     def center(self):
 
@@ -44,14 +57,16 @@ class MainWindow(QWidget, Ui_Form):
 
     def init_item(self):
 
-        for i in range(40):
-            item = QListWidgetItem('item {}'.format(i))
+        for i, name in enumerate(NAME_LIST):
+            NAME_LIST[i] = name.title()
+
+            item = QListWidgetItem(NAME_LIST[i])
             item.setTextAlignment(Qt.AlignCenter)
             item.setFont(QFont('Fixed', 18))
             self.listWidget.addItem(item)
 
-        select_item = self.listWidget.item(18)
-        select_item.setFont(QFont('Fixed', 25))
+        select_item = self.listWidget.item(15)
+        select_item.setFont(QFont('Fixed', 35))
         select_item.setSelected(True)
 
     def list_add(self, file_inf):
@@ -59,23 +74,82 @@ class MainWindow(QWidget, Ui_Form):
         item = QListWidgetItem(file_inf)
         item.setTextAlignment(Qt.AlignCenter)
         self.listWidget.insertItem(0, item)
+        self.listWidget.scrollToTop()
 
-        select_item = self.listWidget.item(18)
-        select_item.setFont(QFont('Fixed', 25))
-        self.listWidget.item(19).setFont(QFont('Fixed', 18))
+        select_item = self.listWidget.item(15)
         select_item.setSelected(True)
-
-        remove_item = self.listWidget.item(40)
-        self.listWidget.removeItemWidget(remove_item)
+        select_item.setFont(QFont('Fixed', 35))
+        self.listWidget.item(16).setFont(QFont('Fixed', 18))
 
     def wheel_run(self):
 
-        self.StartButton.setText('stop')
-        self.thread.start()
+        global THREAD_RUN, SPEED, RESULT
 
-    def print_selected(self):
+        if self.StartButton.text() == '开始':
+            self.StartButton.setText('停止')
 
-        print(self.listWidget.selectedItems()[0].text())
+            THREAD_RUN = True
+            SPEED = 0.05
+            self.thread.start()
+        else:
+            THREAD_RUN = False
+            RESULT = False
+
+            self.StartButton.setText('禁')
+            self.StartButton.setEnabled(False)
+            self.timer.start(1000)
+
+    def group_add(self):
+
+        global RESULT
+
+        if RESULT:
+            self.StartButton.setEnabled(True)
+            self.StartButton.setText('开始')
+
+            while self.listWidget.item(40):
+                self.listWidget.takeItem(40)
+
+            text = self.listWidget.selectedItems()[0].text()
+            NAME_LIST.remove(text)
+            GROUP_LIST.append(text)
+
+            item = QTableWidgetItem(text)
+            item.setTextAlignment(Qt.AlignCenter)
+            self.tableWidget.setItem(self.row, self.col, item)
+            self.tableWidget.resizeColumnsToContents()
+
+            if self.col:
+                self.col += 1
+            else:
+                self.row += 1
+                self.col -= 1
+
+            self.timer.stop()
+
+    def reset(self):
+
+        if len(GROUP_LIST) == 0:
+            QMessageBox.about(self, "莫清了唉， 乖乖", "都没东西了你在清你伟大的母亲大人么？")
+        else:
+            NAME_LIST.extend(GROUP_LIST)
+            GROUP_LIST.clear()
+            self.tableWidget.clearContents()
+            self.row = self.col = 0
+
+    def closeEvent(self, event):
+
+        reply = QMessageBox.information(self,
+                                        "男人都是臭不要脸的",
+                                        "玩完人家就要走，果然男人都是大猪蹄子\r\n你确定不玩了么？",
+                                        QMessageBox.Yes | QMessageBox.No,
+                                        QMessageBox.No
+                                        )
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 
 class WheelThread(QThread):
@@ -87,10 +161,22 @@ class WheelThread(QThread):
         self.num = 0
 
     def run(self):
-        for i in range(1000):
-            list_str = 'Item {}'.format(i)
+
+        global THREAD_RUN, SPEED, RESULT
+        length = len(NAME_LIST)
+
+        while True:
+            list_str = NAME_LIST[self.num % length]
             self.sin_out.emit(list_str)
-            time.sleep(0.05)
+            if not THREAD_RUN:
+                SPEED *= 1.05
+                if SPEED > 0.6:
+                    break
+            time.sleep(SPEED)
+            self.num += 1
+
+        self.num %= length
+        RESULT = True
 
 
 if __name__ == "__main__":
@@ -98,4 +184,3 @@ if __name__ == "__main__":
     FORM = MainWindow()
     FORM.show()
     sys.exit(APP.exec_())
-
